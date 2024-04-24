39db11f74aa3078f37ef80913bdfbfe067099cda
liaStratos <167949837+liaStratos@users.noreply.github.com>
2024-04-23 23:53:01 -0500

    added docstrings

--- television.py
+++ television.py
@@ -5,26 +5,54 @@ class Television:
     MAX_CHANNEL = 3
 
     def __init__(self):
+        """
+        Function defines the initial settings fot the variable
+        param a: status on/off
+        param b: muted on/off
+        param c: volume set at minimum of 0
+        param d: channel set at minimum of 0
+        """
+
         self.status = False
         self.muted = False
         self.volume = self.MIN_VOLUME
         self.channel = self.MIN_CHANNEL
 
     def power(self):
+        """
+        Function initialized the status variable
+        param a: status of the television off
+        """
         self.status = not self.status
 
     def mute(self):
+        """
+        Function initialized the muted variable
+        param a: mute of the television off
+        """
         self.muted = not self.muted
 
     def channel_up(self):
+        """
+        Function makes the channel increase on call
+        param a: channel of the tv, increases by 1
+        """
         if self.status:
             self.channel = (self.channel + 1) % (self.MAX_CHANNEL + 1)
 
     def channel_down(self):
+        """
+        Function makes the channel decrease on call
+        param a: channel of the tv, decreases by 1
+        """
         if self.status:
             self.channel = (self.channel - 1) % (self.MAX_CHANNEL + 1)
 
     def volume_up(self):
+        """
+        Function makes the volume increase on call
+        param a: volume of the tv, increases by 1
+        """
         if self.status:
             if self.volume < self.MAX_VOLUME:
                 self.volume += 1
@@ -32,6 +60,10 @@ class Television:
                     self.muted = False
 
     def volume_down(self):
+        """
+        Function makes the volume decrease on call
+        param a: volume of the tv, decreases by 1
+        """
         if self.status:
             if self.volume > self.MIN_VOLUME:
                 self.volume -= 1
